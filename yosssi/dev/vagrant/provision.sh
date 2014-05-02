86e2d524070ac8e791d03d6f1cec0f2a9a8a1db8
yosssi <yoshida.keiji.84@gmail.com>
2014-05-02 11:25:23 +0900

    Updated yosssi/dev.

--- yosssi/dev/vagrant/provision.sh
+++ yosssi/dev/vagrant/provision.sh
@@ -22,3 +22,18 @@ rm /usr/local/go1.3beta1.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> $BASH_PROFILE
 echo "export GOPATH=$GO_HOME" >> $BASH_PROFILE
 echo "export PATH=\$PATH:\$GOPATH/bin\$GOROOT:\$GOROOT/bin" >> $BASH_PROFILE
+
+# Install Java 7 Update 55
+curl -o /usr/local/lib/jre-7u51-linux-x64.tar.gz https://s3-ap-northeast-1.amazonaws.com/yosssi/java/jre-7u55-linux-x64.gz
+tar -C /usr/local/lib -xzf /usr/local/lib/jre-7u51-linux-x64.tar.gz
+rm /usr/local/lib/jre-7u51-linux-x64.tar.gz
+echo "export JAVA_HOME=/usr/local/lib/jre1.7.0_55" >> $BASH_PROFILE
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> $BASH_PROFILE
+
+# Install Elasticsearch 1.1.1
+curl -o /usr/local/lib/elasticsearch-1.1.1.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.tar.gz
+tar -C /usr/local/lib -xzf /usr/local/lib/elasticsearch-1.1.1.tar.gz
+rm /usr/local/lib/elasticsearch-1.1.1.tar.gz
+echo "export ELASTICSEARCH_HOME=/usr/local/lib/elasticsearch-1.1.1" >> $BASH_PROFILE
+echo "export PATH=\$PATH:\$ELASTICSEARCH_HOME/bin" >> $BASH_PROFILE
+chmod 777 /usr/local/lib/elasticsearch-1.1.1
