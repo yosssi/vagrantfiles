a468aa276a894aa67d4152ed20f78896d741845b
yosssi <yoshida.keiji.84@gmail.com>
2014-08-24 11:33:20 +0900

    Add yosss/rails

--- yosssi/rails/vagrant/provision.sh
+++ yosssi/rails/vagrant/provision.sh
@@ -7,12 +7,6 @@ GO_HOME=/host/go
 GO_VERSION=go1.3.1
 GO_FILE_NAME=$GO_VERSION.linux-amd64.tar.gz
 
-JAVA_FILE_NAME=jre-7u55-linux-x64.gz
-JAVA_DIRECTORY_NAME=jre1.7.0_55
-
-ELASTICSEARCH_DIRECTORY_NAME=elasticsearch-1.1.1
-ELASTICSEARCH_FILE_NAME=${ELASTICSEARCH_DIRECTORY_NAME}.tar.gz
-
 RBENV_SH=/etc/profile.d/rbenv.sh
 
 RUBY_VERSION=2.1.2
@@ -40,33 +34,6 @@ echo "export GOROOT=/usr/local/go" >> $BASH_PROFILE
 echo "export GOPATH=$GO_HOME" >> $BASH_PROFILE
 echo "export PATH=\$PATH:\$GOPATH/bin\$GOROOT:\$GOROOT/bin" >> $BASH_PROFILE
 . $BASH_PROFILE
-
-# Install Java
-curl -o /usr/local/lib/$JAVA_FILE_NAME https://s3-ap-northeast-1.amazonaws.com/yosssi/java/$JAVA_FILE_NAME
-tar -C /usr/local/lib -xzf /usr/local/lib/$JAVA_FILE_NAME
-rm /usr/local/lib/$JAVA_FILE_NAME
-echo "export JAVA_HOME=/usr/local/lib/$JAVA_DIRECTORY_NAME" >> $BASH_PROFILE
-echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> $BASH_PROFILE
-. $BASH_PROFILE
-
-# Install Elasticsearch
-curl -o /usr/local/lib/$ELASTICSEARCH_FILE_NAME https://download.elasticsearch.org/elasticsearch/elasticsearch/$ELASTICSEARCH_FILE_NAME
-tar -C /usr/local/lib -xzf /usr/local/lib/$ELASTICSEARCH_FILE_NAME
-rm /usr/local/lib/$ELASTICSEARCH_FILE_NAME
-echo "export ELASTICSEARCH_HOME=/usr/local/lib/$ELASTICSEARCH_DIRECTORY_NAME" >> $BASH_PROFILE
-echo "export PATH=\$PATH:\$ELASTICSEARCH_HOME/bin" >> $BASH_PROFILE
-. $BASH_PROFILE
-chown vagrant:vagrant -R $ELASTICSEARCH_HOME
-
-# Install Elasticsearch plugins
-## Install Marvel
-$ELASTICSEARCH_HOME/bin/plugin -i elasticsearch/marvel/latest
-## Install elasticsearch-inquisitor
-$ELASTICSEARCH_HOME/bin/plugin -i polyfractal/elasticsearch-inquisitor
-## Install Japanese (kuromoji) Analysis
-$ELASTICSEARCH_HOME/bin/plugin -i elasticsearch/elasticsearch-analysis-kuromoji/2.1.0
-## Install elasticsearch-head
-$ELASTICSEARCH_HOME/bin/plugin -i mobz/elasticsearch-head
 
 # Install rbenv
 git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
